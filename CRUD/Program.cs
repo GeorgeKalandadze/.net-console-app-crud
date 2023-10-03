2b1d69a3cdfec8c3b90f09daef6ec93878c5fd2f
George Kalandadze <kalandadzeg777@gmail.com>
2023-10-03 21:08:48 +0400

    feat:starting making of crud in .net console app

---
+++ CRUD/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = @"Data Source=DESKTOP-URAA2EI\SQLEXPRESS;Initial Catalog=online-magazine;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                string sqlQuery = "SELECT p.Name AS ProductName, c.Name AS CategoryName, p.Price " +
+                                  "FROM products p " +
+                                  "INNER JOIN categories c ON p.category_ID = c.ID";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string productName = (string)reader["ProductName"];
+                            string categoryName = (string)reader["CategoryName"];
+                            decimal price = (decimal)reader["Price"];
+
+                            string rowData = $"Product Name: {productName}, Category Name: {categoryName}, Price: {price:C}";
+                            Console.WriteLine(rowData);
+                        }
+                    }
+                }
+
+                connection.Close();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
